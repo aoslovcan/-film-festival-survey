3f5ae8c07ab47fe39a5980f9421d0057e3a12da7
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-11-18 13:36:05 +0100

    add survey client

---
+++ src/api/SurveyClient/SurveyClient.ts
@@ -0,0 +1,18 @@
+import { ApiClient } from '../ApiClient/ApiClient';
+import {constants} from '../../constants/constants';
+
+export class SurveyClient extends ApiClient{
+
+  getSurvey() : Promise<Record<string, unknown>> {
+    const url = `${constants.BASE_URL}/survey`;
+    const data: Record<string, unknown> = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return this.fetchApi(url, data);
+  }
+}
---
+++ src/constants/constants.ts
@@ -0,0 +1,3 @@
+export const constants =  Object.freeze({
+  BASE_URL : '/api/v1'
+})
