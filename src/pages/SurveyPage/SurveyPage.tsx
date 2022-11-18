4ca9ab2bdc14a8bc6bfd079560d2e5e5c7ba7804
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-11-18 17:34:42 +0100

    call survey api and implement form

--- src/pages/SurveyPage/SurveyPage.tsx
+++ src/pages/SurveyPage/SurveyPage.tsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SurveyForm } from '../../components/SurveyForm/SurveyForm'
+import './SurveyPage.css'
+import { SurveyClient } from '../../api/SurveyClient/SurveyClient'
 
 export const SurveyPage = () => {
-  return <p>Survey goes here</p>
+  const [surveyData, setSurveyData] = useState<Record<string, unknown> | any>()
+  const surveyClient = new SurveyClient()
+
+  const getSurveyData = () => {
+    surveyClient.getSurvey().then((res) => setSurveyData(res.data))
+  }
+
+  useEffect(() => {
+    getSurveyData()
+  }, [])
+
+  const description = surveyData?.attributes.description
+
+  return (
+    <div className='container'>
+      <div className='survey-header'>
+        <h3>{surveyData?.attributes.title}</h3>
+        {description}
+      </div>
+      <SurveyForm questions={surveyData?.attributes.questions} />
+    </div>
+  )
 }
