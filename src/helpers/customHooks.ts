733e5e554d5113d3aac5bed9de838104f6bb5ffe
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-11-18 17:32:10 +0100

    add custom hook for focus

---
+++ src/helpers/customHooks.ts
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react'
+
+export const useFocus = () => {
+  const focusElement = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    focusElement.current?.focus();
+  });
+
+  return focusElement;
+};
