553e694e708a0f0d33cde7e2f6fe9cd14275b35d
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-11-25 15:16:58 +0100

    create success page and style

--- src/pages/SuccessPage/SuccessPage.tsx
+++ src/pages/SuccessPage/SuccessPage.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import { useLocation } from 'react-router'
+import './SuccessPage.css'
+import { Answer } from '../../types/types'
+import { arrayOfNumber } from '../../helpers/commonFunc'
+import { StarItem } from '../../components/SurveyForm/RatingArea/StarItem/StarItem'
 
 export const SuccessPage = () => {
-  return <div className='container'></div>
+  const location = useLocation()
+
+  const createStars = (n: number) => {
+    return arrayOfNumber(n).map((i) => <StarItem color='orange' />)
+  }
+
+  return (
+    <div className='container'>
+      <div className='spaceBetween' />
+      <div className='successfullyContent'>
+        <p className='successfullyMessage'>Thank you for your feedback!!!</p>
+        <div className='answers'>
+          {location.state.attributes.answers.map((answer: Answer, i: number) => (
+            <span key={i}>
+              {typeof answer.answer === 'number' ? createStars(answer.answer) : answer.answer}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
