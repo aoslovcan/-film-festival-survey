90ecd5711ffb2325e3c0c6676202b5b70aaf58d1
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-11-25 15:15:18 +0100

    add color prop in star component

--- src/components/SurveyForm/RatingArea/StarItem/StarItem.tsx
+++ src/components/SurveyForm/RatingArea/StarItem/StarItem.tsx
@@ -3,14 +3,27 @@ import { FaStar } from 'react-icons/fa'
 import './StarItem.css'
 
 interface StarItemProps {
-  clicked: boolean
-  onChange: React.MouseEventHandler
+  clicked?: boolean
+  onChange?: React.MouseEventHandler
+  color?: string
 }
 
-export const StarItem: FC<StarItemProps> = ({ clicked, onChange }) => {
+export const StarItem: FC<StarItemProps> = ({ clicked, onChange, color }) => {
+  const setStarColor = () => {
+    if (color) {
+      return color
+    }
+
+    if (clicked) {
+      return '#ffa500'
+    }
+
+    return '#808080'
+  }
+
   return (
     <>
-      <FaStar onClick={onChange} color={clicked ? '#ffa500' : '#808080'} />
+      <FaStar onClick={onChange} color={setStarColor()} />
     </>
   )
 }
